55dee2ef47c435f087102109238074ac6e27f824
Mauricio Leal <Mauricio.AndresLealVillarael@classmates.com>
2017-06-09 11:46:27 -0500

    Created a "src" directory and moved all .js inside.

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ let app = express();
 app.set('port', (process.env.PORT || 5000));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
 // Middlewares
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
 require('./routes/router')(app, express);
